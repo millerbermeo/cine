24d61bd5919ccd7e8f1280067de82dff7d023cd1
millerbermeo <millerbermeo30@gmail.com>
2025-03-17 13:01:06 -0500

    estilos card altura

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -38,7 +38,7 @@
         document.getElementById("peliculasContainer").innerHTML = peliculas
             .map(
                 (pelicula) => `
-                <div class="card card-side border-gray-300 border bg-base-100 shadow max-h-[155px]">
+                <div class="card card-side border-gray-300 border bg-base-100 shadow max-h-[200px]">
                     <figure>
                         <img class="h-[150px] ${pelicula.foto ? '' : 'hidden'} w-[120px] bg-white object-cover" src="storage/${pelicula.foto}" />
                     </figure>
